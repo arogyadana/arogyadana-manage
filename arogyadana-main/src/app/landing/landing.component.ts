d199c21225e08efea1226ed4176b25f3e6ccd511
“hemanthreddy31” <hemanthkankanala1@gmail.com>
2025-09-24 23:17:45 +0530

    landing pages

---
+++ arogyadana-main/src/app/landing/landing.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LandingComponent {}
